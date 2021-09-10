77f7dbdb64b6f9efd924187c12d16eaea0da83e1
Himanshu Pal <64102513+Royal-Pal@users.noreply.github.com>
2021-09-10 10:05:40 +0530

    Time: 4 ms (88.48%), Space: 9.1 MB (12.51%) - LeetHub

---
+++ move-zeroes/move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        
+        int low = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != 0) {
+                swap(nums[i], nums[low]);
+                low++;
+            }
+        }
+        
+        return;
+    }
+};
